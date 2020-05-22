4a6951f50648a5fc09915b3720e9ea13bf1e7fba
Daniel Oyagha <daniel.oyagha@gmail.com>
2020-05-22 10:37:53 +0100

    project set up commit

---
+++ BukaTeria/Controllers/BukaController.cs
@@ -0,0 +1,18 @@
+﻿using BukaTeria.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BukaTeria.Controllers
+{
+    public class BukaController : Controller
+
+    {
+        public BukaController(IBukaService bukaService)
+        {
+
+        }
+    }
+}
---
+++ BukaTeria/Services/IBukaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BukaTeria.Services
+{
+    public interface IBukaService
+    {
+    }
+}
